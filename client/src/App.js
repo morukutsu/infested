2e33f0e04e5c810acc4ad21c842affa554431bb5
Morukutsu <ulderic.k@free.fr>
2016-12-13 02:57:35 +0100

    [feature] initial work to make Phaser work with the refactoring

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import Game from './Game';
+
 class App extends Component {
     render() {
         return (
--- client/src/Game.js
+++ client/src/Game.js
@@ -2,26 +2,38 @@
 * project_infested client
 * Game.js - Phaser initialization
 */
-require(['states/WorldState', 'Phaser'], function(WorldState) {
-    "use strict";
+/*import 'phaser/src/pixi/Pixi.js';
+import './p2';
+import Phaser from 'phaser/build/custom/phaser-split.js';*/
 
+import Phaser from 'phaser-shim';
+
+/*global.PIXI = require('phaser/src/pixi/Pixi.js');
+global.p2 = require('p2');
+global.Phaser = require('phaser');*/
+
+
+/*require(['states/WorldState', 'Phaser'], function(WorldState) {
+    "use strict";
+*/
     // Configure screen resolution
     var SCREEN_WIDTH  = 1280;
     var SCREEN_HEIGHT = 720;
     var CLIENT_VERSION = '0.0.1';
 
     // Create default state (WorldState)
-    var worldState = new WorldState();
+    //var worldState = new WorldState();
 
     // Prepare scaling factors setup (2x scale)
     var pixel = { scale: 2, canvas: null, context: null, width: 0, height: 0 };
     pixel.width = SCREEN_WIDTH;
     pixel.height = SCREEN_HEIGHT;
 
-    worldState.pixel = pixel;
+    //worldState.pixel = pixel;
 
     // Create main game object
-    var game = new Phaser.Game(pixel.width / pixel.scale, pixel.height / pixel.scale, Phaser.CANVAS, '', worldState);
+    //console.log(Phaser);
+    var game = new Phaser.Game(pixel.width / pixel.scale, pixel.height / pixel.scale, Phaser.CANVAS, '', /*worldState*/ null);
 
     // Setup new scaled canvas and hide the non-scaled one
     //pixel.canvas = Phaser.Canvas.create(pixel.width * pixel.scale, pixel.height * pixel.scale);
@@ -30,4 +42,4 @@ require(['states/WorldState', 'Phaser'], function(WorldState) {
 
     // Disable smoothing for pixel perfect rendering of our pixel art assets
     //Phaser.Canvas.setSmoothingEnabled(pixel.context, false);
-});
+//});
